3af09fd63ce5fbb1ecbaeb4d4be1bf080433cfcd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-07-10 21:42:59 -0400

    feat(debug_protocol): add a structured value type

--- src/debug_protocol.ml
+++ src/debug_protocol.ml
@@ -175,3 +175,128 @@ let debug_read (toks : Token.t list) : protocol_read =
   (module M)
 
 [@@@ocaml.warning "+8"]
+
+module Value = struct
+  type t =
+    | V_struct of struct_
+    | V_list of element_type * t list
+    | V_set of element_type * t list
+    | V_map of element_type * element_type * (t * t) list
+    | V_bool of bool
+    | V_byte of char
+    | V_i16 of int
+    | V_i32 of int32
+    | V_i64 of int64
+    | V_double of float
+    | V_string of string
+    | V_binary of string
+
+  and field = { f_id: field_id; f_ty: field_type; f_v: t }
+  and struct_ = { s_name: string; s_fields: field list }
+
+  type msg = {
+    name: string;
+    ty: message_type;
+    seq_num: sequence_number;
+    s: struct_;
+  }
+  (** A message *)
+
+  let rec pp out (self : t) : unit =
+    let fpf = Format.fprintf in
+    match self with
+    | V_bool b -> fpf out "%B" b
+    | V_byte c -> fpf out "%C" c
+    | V_i16 i -> fpf out "%d" i
+    | V_i32 i -> fpf out "%ldl" i
+    | V_i64 i -> fpf out "%LdL" i
+    | V_double f -> fpf out "%f" f
+    | V_string s -> fpf out "%S" s
+    | V_binary s -> fpf out "binary(%S)" s
+    | V_struct s -> pp_struct out s
+    | V_list (ty, l) ->
+      fpf out "list %s {@[" (string_of_element_type ty);
+      List.iter (fpf out "%a;@ " pp) l;
+      fpf out "@]}"
+    | V_set (ty, l) ->
+      fpf out "set %s {@[" (string_of_element_type ty);
+      List.iter (fpf out "%a;@ " pp) l;
+      fpf out "@]}"
+    | V_map (ty1, ty2, l) ->
+      fpf out "map %s %s{@["
+        (string_of_element_type ty1)
+        (string_of_element_type ty2);
+      List.iter (fun (k, v) -> fpf out "@[%a ->@ %a@];@ " pp k pp v) l;
+      fpf out "@]}"
+
+  and pp_field out (f : field) =
+    Format.fprintf out "@[<2>[%d: %s] = %a@]" f.f_id
+      (string_of_field_type f.f_ty)
+      pp f.f_v
+
+  and pp_struct out (s : struct_) =
+    Format.fprintf out "@[<2>struct %s {@ " s.s_name;
+    List.iter (Format.fprintf out "%a;@ " pp_field) s.s_fields;
+    Format.fprintf out "@;<1 -2>}@]"
+
+  let pp_msg out (m : msg) : unit =
+    Format.fprintf out
+      "@[<hv2>msg {@ name=%S;@ ty=%s;@ seq_num=%d@ str=%a@;<1 -2>}@]" m.name
+      (string_of_message_type m.ty)
+      m.seq_num pp_struct m.s
+
+  let to_tok_list (self : t) : Token.t list =
+    let toks = ref [] in
+    let add x = toks := x :: !toks in
+
+    let rec aux =
+      let open Token in
+      function
+      | V_bool b -> add @@ T_bool b
+      | V_byte c -> add @@ T_byte c
+      | V_i16 i -> add @@ T_i16 i
+      | V_i32 i -> add @@ T_i32 i
+      | V_i64 i -> add @@ T_i64 i
+      | V_double f -> add @@ T_double f
+      | V_string s -> add @@ T_string s
+      | V_binary s -> add @@ T_binary s
+      | V_struct s -> aux_struct s
+      | V_list (ty, l) ->
+        add @@ T_list_begin (ty, List.length l);
+        List.iter aux l;
+        add @@ T_list_end
+      | V_set (ty, l) ->
+        add @@ T_set_begin (ty, List.length l);
+        List.iter aux l;
+        add @@ T_set_end
+      | V_map (ty1, ty2, l) ->
+        add @@ T_map_begin (ty1, ty2, List.length l);
+        List.iter
+          (fun (k, v) ->
+            aux k;
+            aux v)
+          l;
+        add @@ T_map_end
+    and aux_struct s =
+      let open Token in
+      add @@ T_struct_begin s.s_name;
+      List.iter
+        (fun f ->
+          add @@ T_field_begin ("", f.f_ty, f.f_id);
+          aux f.f_v;
+          add @@ T_field_end)
+        s.s_fields;
+      add @@ T_field_stop;
+      add @@ T_struct_end
+    in
+
+    aux self;
+    List.rev !toks
+
+  let msg_to_tok_list (self : msg) : Token.t list =
+    let open Token in
+    [ T_msg_begin (self.name, self.ty, self.seq_num); T_msg_end ]
+    @ to_tok_list (V_struct self.s)
+
+  (* TODO: of_tok_list *)
+end
